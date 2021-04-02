af6b40b687ad6d16e12dd07162498ca82de3ea3d
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-04-01 22:35:06 -0500

    render license section done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,12 +15,23 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) { 
-  if(license)
+  if(license==='Apache'){
+    return `[![License](${renderLicenseBadge(license)})](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license==='GNU GPLv3'){
+    return `[![License](${renderLicenseBadge(license)})](http://www.gnu.org/licenses/gpl-3.0)`
+
+  } else if (license==='MIT'){
+    return `[![License](${renderLicenseBadge(license)})](https://opensource.org/licenses/MIT)`
+  } else if (license==="ISC"){
+    return `[![License](${renderLicenseBadge(license)})](https://opensource.org/licenses/ISC)`
+  } else { return "" }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) { 
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
