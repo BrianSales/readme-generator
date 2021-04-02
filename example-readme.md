42f7bce6603c626cd2ee5be57ec979df9bf2cd67
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-04-01 23:20:52 -0500

    added error handeling to all questions

---
+++ example-readme.md
@@ -0,0 +1,36 @@
+# GreenBay [![License](https://img.shields.io/static/v1?label=License&message=ISC&color=blue)](https://opensource.org/licenses/ISC)
+
+  # Table of Contents
+  * [Project Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  # Description
+  A brief history about the football team
+
+  # Installation
+  User can look up facts about the Greenbay Packers
+
+  # Usage
+  Fun informational tool
+
+  # Contributions
+  User must click a button to display random fact about the greenbay packers
+
+  # Tests
+  Npm run test
+
+  # License
+  [Released under the ISC license](https://opensource.org/licenses/ISC)
+
+  # Questions
+  If you have any questions, you can reach me at [mr.brian.mrs.kathleen@gmail.com](mr.brian.mrs.kathleen@gmail.com)
+  or you can reach me at at my [github repo](https://github.com/BrianSales)
+
+
+
+
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 const { message } = require('statuses')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
@@ -17,27 +18,32 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Enter your project description"
+        message: "Enter your project description",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "instructions",
-        message: "Enter your project installation instructions"
+        message: "Enter your project installation instructions",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "usage",
-        message: "Enter your project usage"
+        message: "Enter your project usage",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "contribution",
-        message: "Enter your project contribution guidelines"
+        message: "Enter your project contribution guidelines",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "test",
-        message: "Enter your project's test instructions"
+        message: "Enter your project's test instructions",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "list",
@@ -46,23 +52,33 @@ const questions = [
         choices: [
             "Apache", "GNU GPLv3", "MIT", "ISC", "None"
 
-        ]
+        ],
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "username",
-        message: "Enter your github user name"
+        message: "Enter your github user name",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: "input",
         name: "email",
-        message: "Enter email address"
+        message: "Enter email address",
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     }
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if (err){
+            console.log(err)
+        }
+        console.log("readme generated succesfully")
+    })
+ }
 
 // TODO: Create a function to initialize app
 function init() {
